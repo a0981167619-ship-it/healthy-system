388b6a3dcd2bb12a67f0bfa31571e00c7c97907b
K <a0981167619@gmail.com>
2026-07-14 22:40:55 +0800

    追加新資料-a+

---
+++ a++.py
@@ -0,0 +1,7 @@
+we arrival
+we destination is in front of hospital.
+we destination is in front of hospital.
+we destination is in front of hospital.
+ uh...
+we destination is in front of hospital.
+ uh...
